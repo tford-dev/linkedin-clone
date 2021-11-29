2f0c12b1dee4b57da2da1221d8d7a14001752fcf
tford-dev <terranceford513@gmail.com>
2021-11-29 13:18:50 -0500

    Added styled-components dependency and made component for login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React from 'react'
+import {BrowserRouter as Router, Route, Routes as Switch} from "react-router-dom";
 import './App.css';
+import Login from './Components/Login';
 
 function App() {
     return (
 		<div>
-			<h1>Linkedin clone</h1>
+			<Router>
+				<Switch>
+					<Route exact path="Login" element={<Login />} />
+				</Switch>
+			</Router>
 		</div>
     );
 }
---
+++ src/Components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+import './Login.css';
+
+export const Login = (props) => {
+    return (
+        <div>
+            Login
+        </div>
+    )
+}
