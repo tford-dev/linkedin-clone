1533df63e3ef319616b3aaa876eea93e99deb0ef
tford-dev <terranceford513@gmail.com>
2021-11-29 14:42:00 -0500

    Converted svgs to png and started nav to login component

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
-import './Login.css';
 
-export const Login = (props) => {
+const Login = (props) => {
     return (
-        <div>
-            Login
-        </div>
+        <Container>
+            <Nav>
+                <a href="/">
+                    <img src="project-img/login-logo.png" alt="login-img"/>
+                </a>
+            </Nav>
+        </Container>
     )
 }
+
+const Container= styled.div`
+    padding: 0px;
+`;
+const Nav = styled.nav`
+    max-width: 1128px;
+    margin: auto;
+    paddingL 12px 0 16px;
+    display: flex;
+    align-items: center;
+    position: relative;
+    justify-content: space-between;
+    flex-wrap: nowrap;
+
+    & > a {
+        width: 135px;
+        height: 34px;
+        @media (max-width: 768px) {
+            padding: 0 5px;
+        }
+    }
+`;
+
+export default Login;
