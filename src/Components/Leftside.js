dfabfd70817ab8be58db26ee7ab04e767416366f
tford-dev <terranceford513@gmail.com>
2021-11-30 14:36:19 -0500

    Added and defined Container, ArtCard, UserInfo, CardBg, Photo, Link, and AddPhototxt tags to Leftside.js

--- src/Components/Leftside.js
+++ src/Components/Leftside.js
@@ -4,10 +4,35 @@ import styled from "styled-components";
 
 function Leftside() {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <ArtCard>
+                <UserInfo>
+                    <CardBackground />
+                    <a>
+                        <Photo />
+                        <Link>Welcome, there!</Link>
+                    </a>
+                    <a>
+                        <AddPhotoText>Add a photo</AddPhotoText>
+                    </a>
+                </UserInfo>
+            </ArtCard>
+        </Container>
     )
 }
 
-export default Leftside
+const Container = styled.div``;
+
+const ArtCard = styled.div``;
+
+const UserInfo = styled.div``;
+
+const CardBackground = styled.div``;
+
+const Photo = styled.div``;
+
+const Link = styled.div``;
+
+const AddPhotoText = styled.div``;
+
+export default Leftside;
