4ca20607ea52cbb22b971be0c0c4087d8d30aa66
tford-dev <terranceford513@gmail.com>
2021-11-30 13:10:45 -0500

    Styled NavList tag

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -171,7 +171,43 @@ const NavListWrap = styled.ul`
     }
 `;
 
-const NavList = styled.li``;
+const NavList = styled.li`
+    display: flex;
+    align-items: center;
+    a {
+        align-items: center;
+        background: transparent;
+        display: flex;
+        flex-direction: column;
+        font-size: 12px;
+        font-weight: 400;
+        justify-content: center;
+        line-height: 1.5;
+        min-height: 52px;
+        min-width: 80px;
+        position: relative;
+        text-decoration: none;
+
+        span {
+            color: rgba(0, 0, 0, 0.6);
+            display: flex;
+            align-items: center;
+        }
+
+        @media (max-width: 768px) {
+            min-width: 70px;
+        }
+    }
+
+    &:hover,
+    &:active {
+        a {
+            span {
+                color: rgba(0, 0, 0, 0.9)
+            }
+        }
+    }
+`;
 
 const SignOut = styled.div``;
 
