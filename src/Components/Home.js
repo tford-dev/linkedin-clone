0f3f8a2dae2e60f718f38595d1e1027e4c83b069
tford-dev <terranceford513@gmail.com>
2021-11-30 14:20:06 -0500

    Styled Layout Component and added Righside, Main, and Leftside files

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from "styled-components";
 import Header from "./Header"
 
-function Home() {
+function Home(props) {
     return (
         <Container>
             <Header />
@@ -15,12 +15,11 @@ function Home() {
                 </p>
             </Section>
             <Layout>
+
             </Layout>
         </Container>
     )
 }
-
-export default Home
 
 const Container = styled.div`
     padding-top: 52px;
@@ -61,3 +60,19 @@ const Section = styled.section`
     }
 `;
 
+const Layout = styled.div`
+    display: grid;
+    grid-template-areas: "leftside main rightside";
+    grid-template-columns: minmax(0, 5fr) minmax()(0, 12fr) minmax(300px, 7fr);
+    column-gap: 25px;
+    row-gap: 25px;
+    /* grid-template-row: auto */
+    margin: 25px 0px;
+    @media (max-width: 768px) {
+        display: flex;
+        flex-direction: column;
+        padding: 0px 5px;
+    }
+`;
+
+export default Home;
