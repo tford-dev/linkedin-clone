7d44fd2509d9ac1cd5b41658898d86351de027e1
tford-dev <terranceford513@gmail.com>
2021-11-30 14:11:52 -0500

    Styled Section tag

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -32,3 +32,32 @@ const Container = styled.div`
 //     margin-left: auto;
 //     margin-right: auto;
 // `
+
+const Section = styled.section`
+    min-height: 50px;
+    padding: 16px 0;
+    box-sizing: content-box;
+    text-align: center;
+    text-decoration: underline;
+    display: flex;
+    justify-content: center;
+    h5 {
+        color: #0a66c2;
+        font-size: 14px;
+        a {
+            font-weight: 700;
+        }
+    }
+
+    p {
+        font-size: 14px;
+        color: #434649;
+        font-weight: 600;
+    }
+
+    @media (max-width: 768px){
+        flex-direction: column;
+        padding: 0px 5px;
+    }
+`;
+
