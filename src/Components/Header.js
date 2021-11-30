676318116cb94109cd5f140d0e8641e33aaf4cb0
tford-dev <terranceford513@gmail.com>
2021-11-30 12:00:15 -0500

    Styled Container tag

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -16,7 +16,16 @@ const Header = (props) => {
 
 export default Header;
 
-const Container = styled.div``
+const Container = styled.div`
+    background-color: white;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.08);
+    left: 0;
+    padding: 0px 24px;
+    position: fixed;
+    top: 0;
+    width: 100vw;
+    z-index: 100;
+`
 
 const Content = styled.div``
 
