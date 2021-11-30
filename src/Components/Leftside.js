0bfe136662b8b2a0767e318a59a197aaa077df85
tford-dev <terranceford513@gmail.com>
2021-11-30 14:39:37 -0500

    Styled Container and ArtCard tag

--- src/Components/Leftside.js
+++ src/Components/Leftside.js
@@ -21,9 +21,21 @@ function Leftside() {
     )
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+    grid-area: leftside;
+`;
 
-const ArtCard = styled.div``;
+const ArtCard = styled.div`
+    text-align: center;
+    overflow: hidden;
+    margin-bottom: 8px;
+    background-color: #fff;
+    border-radius: 5px;
+    transition: box-shadow 83ms;
+    position: relative;
+    border: none;
+    box-shadow: 0px 0px 0px 1px rgb(0 0 0 / 15%), 0px 0px 0px rgb(0 0 0 / 20%);
+`;
 
 const UserInfo = styled.div``;
 
