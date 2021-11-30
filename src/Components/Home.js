e61ff10a4c64d30780787144a25f983492bba00c
tford-dev <terranceford513@gmail.com>
2021-11-30 14:07:52 -0500

    Styled Container and Content tag

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,11 +6,29 @@ function Home() {
     return (
         <Container>
             <Header />
-
+            <Section>
+                <h5>
+                    <a>Hiring in a hurry? - </a>
+                </h5>
+                <p>
+                    Find talented pros in record time with Upwork and keep business moving.
+                </p>
+            </Section>
+            <Layout>
+            </Layout>
         </Container>
     )
 }
 
 export default Home
 
-const Container = styled.div``
+const Container = styled.div`
+    padding-top: 52px;
+    max-width: 100%;
+`;
+
+// const Content = styled.div`
+//     max-width: 1128px;
+//     margin-left: auto;
+//     margin-right: auto;
+// `
