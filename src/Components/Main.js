2036bacb09391a86a3375ff06e0bb705b21872c1
tford-dev <terranceford513@gmail.com>
2021-12-01 11:25:58 -0500

    Started Main.js. Added CommonCard class and Sharebox tag/styled sharebox tag

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,12 +3,43 @@ import styled from "styled-components";
 
 function Main(props) {
     return (
-        <Container>Main</Container>
+        <Container>
+            <ShareBox>Share</ShareBox>
+            <div>
+                <img src="/project-img/user.svg" alt="user" />
+                <button>Share a post</button>
+            </div>
+
+            <div>
+                <button>
+                    <img src="/project-img/photo.svg" alt="photo icon" />
+                </button>
+            </div>
+        </Container>
     )
 };
 
 const Container = styled.div`
     grid-area: main;
 `
+
+const CommonCard = styled.div`
+    text-align: center;
+    overflow: hidden;
+    margin-bottom: 8px;
+    background-color: #fff;
+    border-radius: 5px;
+    position: relative;
+    border: none;
+    border-radius: 0px 0px 0px 1px rgb(0 0 0 / 15%), 0px 0px 0px rgb(0 0 0 / 20%);
+`;
+
+const ShareBox = styled(CommonCard)`
+    display: flex;
+    flex-direction: column;
+    color: #958b7b;
+    margin: 0px 0px 8px 0px;
+    background: #fff;
+`;
 
 export default Main;
