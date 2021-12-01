33c6850f2e90c398ce61f6348b8cb705b26f2a87
tford-dev <terranceford513@gmail.com>
2021-12-01 11:41:28 -0500

    Added buttons and styling for sharebox tag

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -4,17 +4,34 @@ import styled from "styled-components";
 function Main(props) {
     return (
         <Container>
-            <ShareBox>Share</ShareBox>
-            <div>
-                <img src="/project-img/user.svg" alt="user" />
-                <button>Share a post</button>
-            </div>
+            <ShareBox>Share
+                <div>
+                    <img src="/project-img/user.svg" alt="user" />
+                    <button>Share a post</button>
+                </div>
 
-            <div>
-                <button>
-                    <img src="/project-img/photo.svg" alt="photo icon" />
-                </button>
-            </div>
+                <div>
+                    <button>
+                        <img src="/project-img/photo-icon.svg" alt="photo icon" />
+                        <span>Photo</span>
+                    </button>
+
+                    <button>
+                        <img src="/project-img/video-icon.svg" alt="video icon" />
+                        <span>Video</span>
+                    </button>
+
+                    <button>
+                        <img src="/project-img/event-icon.svg" alt="event icon" />
+                        <span>Event</span>
+                    </button>
+
+                    <button>
+                        <img src="/project-img/article-icon.svg" alt="article icon" />
+                        <span>Article</span>
+                    </button>
+                </div>
+            </ShareBox>
         </Container>
     )
 };
@@ -40,6 +57,26 @@ const ShareBox = styled(CommonCard)`
     color: #958b7b;
     margin: 0px 0px 8px 0px;
     background: #fff;
+
+    div {
+        button {
+            outline: none;
+            color: rgba(0, 0, 0, 0.6);
+            font-size: 14px;
+            line-height: 1.5;
+            min-height: 48px;
+            background: transparent;
+            border: none;
+            display: flex;
+            align-items: center;
+            font-weight: 600;
+
+            &:hover {
+                background-color: rgba(0, 0, 0, 0.07);
+                border-radius: 6px;
+            }
+        }
+    }
 `;
 
 export default Main;
