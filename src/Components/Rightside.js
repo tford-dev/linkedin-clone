41dc8192f94eb61a2cf1955069edf85e41270d07
tford-dev <terranceford513@gmail.com>
2021-12-01 09:24:47 -0500

    Styled Container, FollowCard, and Title tags.

--- src/Components/Rightside.js
+++ src/Components/Rightside.js
@@ -6,17 +6,37 @@ function Rightside() {
         <Container>
             <FollowCard>
                 <Title>
-                
+                    <h2>Add to your feed</h2>
+                    <img src="/project-img/feed-icon.svg" alt="" />
                 </Title>
             </FollowCard>
         </Container>
     )
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+    grid-area: rightside;
+`;
 
-const FollowCard = styled.div``;
+const FollowCard = styled.div`
+    text-align: center;
+    overflow: hidden;
+    margin-bottom: 8px;
+    background-color: #fff;
+    border-radius: 5px;
+    position: relative;
+    border: none;
+    box-shadow: 0px 0px 0px 1px rgb(0 0 0 / 15%), 0px 0px 0px rgb(0 0 0 / 20%);
+    padding: 12px;
+`;
 
-const Title = styled.div``;
+const Title = styled.div`
+    display: inline-flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 16px;
+    width: 100%;
+    color: rgba(0, 0, 0, 0.6);
+`;
 
 export default Rightside
