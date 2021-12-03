eeb5cd2d1a2b2625a0d7fa4e9f52533aef697997
tford-dev <terranceford513@gmail.com>
2021-12-03 12:02:58 -0500

    Added methods for PostModal.js

--- src/Components/PostModal.js
+++ src/Components/PostModal.js
@@ -7,6 +7,52 @@ import {postArticleAPI} from '../actions';
 
 
 const PostModal = (props) =>{
+    const [editorText, setEditorText] = useState('');
+    const [sharedImg, setSharedImg] = useState('');
+    const [videoLink, setVideoLink] = useState('');
+    const [assetArea, setAssetArea] = useState('');
+
+    const handleChange = (e) => {
+        const image = e.target.files[0];
+        if(image === '' || image === undefined)
+        {
+            alert(`not an image, the is a ${typeof image}`);
+            return;
+        }
+        setSharedImg(image);
+    };
+
+    const switchAssetArea = (area) => {
+        setSharedImage('');
+        setVideoLink('');
+        setAssetArea(area);
+    };
+
+    const postArticle = (e) => {
+        e.preventDefault();
+        if(e.target !== e.currentTarget){
+            return;
+        }
+
+        const payload = {
+            image: sharedImage,
+            video: videoLink,
+            user: props.user,
+            description: editorText,
+            timestamp: firebase.firestore.Timestamp.now(),
+        };
+        props.postArticle(payload);
+        reset(e);
+    };
+
+    const reset = (e) => {
+        setEditorText("");
+        setSharedImage('');
+        setVideoLink('');
+        setAssetArea('');
+        props.handleClick(e);
+    }
+
     return (
         <div>
             
